19e33734f4ff7afb2526539a85ff85b759361097
Richard Nissen <richardnissenkolding@gmail.com>
2021-11-24 16:20:00 +0100

    added game function, game now works but code readability and efficiency could probably be improved

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -11,6 +11,7 @@ function computerPlay() {
 }
 
 // else statements aren't necessary as return exits the function immediately.
+// Maybe there's a better way of structuring this.
 function playRound(playerSelection, computerSelection) {
     playerSelection = firstLetterUpperCase(playerSelection);
     if (playerSelection == "Rock") {
@@ -25,21 +26,41 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "Rock") {
             return "You Win! Paper beats Rock";
         } else if (computerSelection == "Scissors") {
-            return "You Lose! Scissors beats Paper"
+            return "You Lose! Scissors beats Paper";
         } else {
-            return "Tie! Both picked Paper"
+            return "Tie! Both picked Paper";
         }
     } else if (playerSelection == "Scissors") {
         if (computerSelection == "Rock") {
             return "You Lose! Rock beats Scissors";
         } else if (computerSelection == "Paper") {
-            return "You Win! Scissors beats Paper"
+            return "You Win! Scissors beats Paper";
         } else {
-            return "Tie! Both picked Scissors"
+            return "Tie! Both picked Scissors";
         }
+    } else {
+        return "Please select Rock, Paper or Scissors";
     }
 }
 
 function firstLetterUpperCase(inputString) {
     return inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase();
 }
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 1; i <= 5; i++) {
+        console.log("Game number: " + i);
+        let result = playRound(prompt("Rock, Paper or Scissors?"), computerPlay());
+        if (result.slice(0, 5) == "You W") {
+            playerWins++;
+        } else if (result.slice(0,5) == "You L") {
+            computerWins++;
+        }
+        console.log(result);
+    }
+    return (playerWins > computerWins) ? "Player wins!" :
+        (playerWins < computerWins) ? "Computer wins!" : "Tie"
+}
+console.log(game());
